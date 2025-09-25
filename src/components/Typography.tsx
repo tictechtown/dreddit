57d54d3c47955011f3be3628d6df5b233194cfc2
tictechtown <tictechtown@gmail.com>
2025-09-25 19:45:05 -0400

    Add emphasized variant for Typography

--- src/components/Typography.tsx
+++ src/components/Typography.tsx
@@ -19,6 +19,21 @@ type Props = React.ComponentProps<typeof Text> & {
     | 'labelLarge'
     | 'labelMedium'
     | 'labelSmall'
+    | 'displayLargeEmphasized'
+    | 'displayMediumEmphasized'
+    | 'displaySmallEmphasized'
+    | 'headlineLargeEmphasized'
+    | 'headlineMediumEmphasized'
+    | 'headlineSmallEmphasized'
+    | 'titleLargeEmphasized'
+    | 'titleMediumEmphasized'
+    | 'titleSmallEmphasized'
+    | 'bodyLargeEmphasized'
+    | 'bodyMediumEmphasized'
+    | 'bodySmallEmphasized'
+    | 'labelLargeEmphasized'
+    | 'labelMediumEmphasized'
+    | 'labelSmallEmphasized'
     | 'overline';
   children: React.ReactNode;
   style?: StyleProp<TextStyle>;
@@ -48,17 +63,35 @@ const styles = StyleSheet.create({
     letterSpacing: -0.25,
     fontWeight: '400',
   },
+  displayLargeEmphasized: {
+    fontSize: 57,
+    lineHeight: 64,
+    letterSpacing: -0.25,
+    fontWeight: '500',
+  },
   displayMedium: {
     fontSize: 45,
     lineHeight: 52,
     letterSpacing: 0,
     fontWeight: '400',
   },
+  displayMediumEmphasized: {
+    fontSize: 45,
+    lineHeight: 52,
+    letterSpacing: 0,
+    fontWeight: '500',
+  },
   displaySmall: {
     fontSize: 36,
     lineHeight: 44,
     letterSpacing: 0,
     fontWeight: '400',
+  },
+  displaySmallEmphasized: {
+    fontSize: 36,
+    lineHeight: 44,
+    letterSpacing: 0,
+    fontWeight: '500',
   },
 
   headlineLarge: {
@@ -67,55 +100,111 @@ const styles = StyleSheet.create({
     letterSpacing: 0,
     fontWeight: '400',
   },
+  headlineLargeEmphasized: {
+    fontSize: 32,
+    lineHeight: 40,
+    letterSpacing: 0,
+    fontWeight: '500',
+  },
 
   headlineMedium: {
     fontSize: 28,
     lineHeight: 36,
     fontWeight: '400',
   },
+  headlineMediumEmphasized: {
+    fontSize: 28,
+    lineHeight: 36,
+    fontWeight: '500',
+  },
+
   headlineSmall: {
     fontSize: 24,
     lineHeight: 32,
     fontWeight: '400',
   },
+  headlineSmallEmphasized: {
+    fontSize: 24,
+    lineHeight: 32,
+    fontWeight: '500',
+  },
 
   titleLarge: {
     fontSize: 22,
-    lineHeight: 24,
+    lineHeight: 28,
     letterSpacing: 0,
     fontWeight: '400',
+  },
+  titleLargeEmphasized: {
+    fontSize: 22,
+    lineHeight: 28,
+    letterSpacing: 0,
+    fontWeight: '500',
   },
 
   titleMedium: {
     fontSize: 16,
-    lineHeight: 18,
+    lineHeight: 24,
     letterSpacing: 0.15,
     fontWeight: '500',
+  },
+  titleMediumEmphasized: {
+    fontSize: 16,
+    lineHeight: 24,
+    letterSpacing: 0.15,
+    fontWeight: '700',
   },
 
   titleSmall: {
     fontSize: 14,
-    lineHeight: 18,
+    lineHeight: 20,
     letterSpacing: 0.1,
     fontWeight: '500',
   },
+  titleSmallEmphasized: {
+    fontSize: 14,
+    lineHeight: 20,
+    letterSpacing: 0.1,
+    fontWeight: '700',
+  },
+
   bodyLarge: {
     fontSize: 16,
     lineHeight: 24,
     letterSpacing: 0.5,
     fontWeight: '400',
   },
+  bodyLargeEmphasized: {
+    fontSize: 16,
+    lineHeight: 24,
+    letterSpacing: 0.5,
+    fontWeight: '500',
+  },
+
   bodyMedium: {
     fontSize: 14,
     lineHeight: 20,
     letterSpacing: 0.25,
     fontWeight: '400',
   },
+  bodyMediumEmphasized: {
+    fontSize: 14,
+    lineHeight: 20,
+    letterSpacing: 0.25,
+    fontWeight: '500',
+  },
+
   bodySmall: {
     fontSize: 12,
     lineHeight: 16,
     letterSpacing: 0,
     fontWeight: '400',
+  },
+  bodySmallEmphasized: {
+    fontSize: 12,
+    lineHeight: 16,
+    letterSpacing: 0,
+    fontWeight: '500',
   },
 
   labelLarge: {
@@ -124,12 +213,24 @@ const styles = StyleSheet.create({
     letterSpacing: 0.1,
     fontWeight: '500',
   },
+  labelLargeEmphasized: {
+    fontSize: 14,
+    lineHeight: 20,
+    letterSpacing: 0.1,
+    fontWeight: '700',
+  },
 
   labelMedium: {
     fontSize: 12,
     lineHeight: 16,
     letterSpacing: 0.5,
     fontWeight: '500',
+  },
+  labelMediumEmphasized: {
+    fontSize: 12,
+    lineHeight: 16,
+    letterSpacing: 0.5,
+    fontWeight: '700',
   },
 
   labelSmall: {
@@ -138,6 +239,13 @@ const styles = StyleSheet.create({
     letterSpacing: 0.5,
     fontWeight: '500',
   },
+  labelSmallEmphasized: {
+    fontSize: 11,
+    lineHeight: 16,
+    letterSpacing: 0.5,
+    fontWeight: '700',
+  },
+
   overline: {
     fontSize: 12,
     lineHeight: 16,
